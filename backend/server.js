0a7a8fca2dbcab71440d2e5f6fc8488e0c8b4bfc
Jeremy <jbminerich@hotmail.com>
2024-07-19 10:48:51 -0700

    editing frontpage and adding backend

---
+++ backend/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Replace <raspberry_pi_ip_address> with your Raspberry Pi's IP address
+mongoose.connect('mongodb://<raspberry_pi_ip_address>:27017/notes', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const NoteSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+});
+
+const Note = mongoose.model('Note', NoteSchema);
+
+app.get('/notes', async (req, res) => {
+    const notes = await Note.find();
+    res.send(notes);
+});
+
+app.post('/notes', async (req, res) => {
+    const note = new Note({
+        title: req.body.title,
+        content: req.body.content,
+    });
+    await note.save();
+    res.send(note);
+});
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
+A
+A
+A
+B
+B
+B
+B
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+
